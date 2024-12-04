c1ace9375992779945dd7b54d67338c23fef5458
sekR4 <sebastian.kraus@conplement.de>
2024-12-04 16:34:47 +0100

    finally d4 ✅

--- src/day4.rs
+++ src/day4.rs
@@ -1,3 +1,5 @@
+use std::array;
+use std::collections::btree_set::Range;
 use std::fs::File;
 use std::io::Read;
 
@@ -106,7 +108,76 @@ fn file_2_content(file_path: &str) -> String {
     file_content
 }
 
+fn count_xmas_v2(txt: &str) -> i32 {
+    let matrix = txt_2_matrix(txt);
+    let words_to_find: [&str; 2] = ["MAS", "SAM"];
+    let rows = matrix.len();
+    let cols = matrix[0].len();
+
+    fn x_found(
+        matrix: &Vec<Vec<char>>,
+        x: i32,
+        y: i32,
+        words: [&str; 2],
+        dx: i32,
+        dy: i32,
+        rows: i32,
+        cols: i32,
+    ) -> bool {
+        let mut letters = Vec::new();
+
+        // i = -1, 0, 1
+        for i in -1..2 {
+            let nx = x + dx * i as i32;
+            let ny = y + dy * i as i32;
+            if nx < 0 || ny < 0 || nx >= rows || ny >= cols {
+                return false;
+            }
+            letters.push(matrix[nx as usize][ny as usize]);
+        }
+        let letters_str: String = letters.into_iter().collect();
+        words.contains(&letters_str.as_str())
+    }
+
+    let mut cnt = 0;
+
+    for x in 1..rows - 1 {
+        for y in 1..cols - 1 {
+            // unklar ob das so in rust geht
+            if matrix[x as usize][y as usize] != 'A' {
+                continue;
+            }
+            let dia1 = x_found(
+                &matrix,
+                x as i32,
+                y as i32,
+                words_to_find,
+                1,
+                1,
+                rows as i32,
+                cols as i32,
+            );
+            let dia2 = x_found(
+                &matrix,
+                x as i32,
+                y as i32,
+                words_to_find,
+                1,
+                -1,
+                rows as i32,
+                cols as i32,
+            );
+            if dia1 && dia2 {
+                cnt += 1
+            }
+        }
+    }
+
+    return cnt;
+}
+
 pub fn main() -> i32 {
     let new_txt = file_2_content("src/input/day4.txt");
-    return count_xmas(&new_txt);
+    // return count_xmas(&new_txt);
+    return count_xmas_v2(&new_txt);
 }
